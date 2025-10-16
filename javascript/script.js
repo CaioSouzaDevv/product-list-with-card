daddc87f2cc25f5d2920bee38c73e8f95fc766be
Caiosouzadevv <caioss20.cs@gmail.com>
2025-10-16 08:17:04 -0300

    feat: add product to cart

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,9 @@
+
 const rowProducts = document.querySelector('.row');
 const cardTemplate = document.querySelector('.product');
 const price = document.querySelector('.data');
+
+const listCar = document.querySelector('.cart__list');
 // Remove o card inicial do HTML, para não duplicar
 cardTemplate.remove();
 
@@ -20,7 +23,36 @@ async function loading() {
         cardClone.querySelector('data').textContent = e.price.toFixed(2);
 
         rowProducts.appendChild(cardClone);
+
+        cardClone.querySelector('.product__button').addEventListener('click', function (car) {
+            car.preventDefault();
+
+            const listInCart = document.createElement('li');
+            listInCart.classList.add('cart__item'); // seu bloco BEM
+
+            const itemName = document.createElement('span');
+            itemName.classList.add('cart__item-name');
+            itemName.textContent = e.name;
+
+            const itemPrice = document.createElement('span');
+            itemPrice.classList.add('cart__item-price');
+            itemPrice.textContent = `$${e.price.toFixed(2)}`;
+
+            // Adiciona os dois elementos no li
+            listInCart.appendChild(itemName);
+            listInCart.appendChild(itemPrice);
+
+            // Adiciona o li na lista
+            listCar.appendChild(listInCart);
+
+
+        });
+
     });
+
+
+
+
 }
 
 loading();
